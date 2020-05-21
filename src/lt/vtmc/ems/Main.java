0c6b6b6ecf0950084cadf74935e98c37f119382f
Tomas Putramentas <tomas.putramentas@gmail.com>
2020-05-21 13:22:22 +0300

    Added fix to Main.java

--- src/lt/vtmc/ems/Main.java
+++ src/lt/vtmc/ems/Main.java
@@ -2,9 +2,8 @@ package lt.vtmc.ems;
 
 public class Main {
 
-    private static void main(String... args) throws Exception {
-        // TODO
-        AppMenu menu = new AppMenu();
-        menu.mainMenu();
-    }
+	public static void main(String[] args) throws Exception {
+		AppMenu menu = new AppMenu();
+		menu.mainMenu();
+	}
 }
