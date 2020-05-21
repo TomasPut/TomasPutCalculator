8cad5e7680f0fde7705794e5ccad7cb47840d9a9
Tomas Putramentas <tomas.putramentas@gmail.com>
2020-05-21 14:04:55 +0300

    Last empty commit

--- src/lt/vtmc/ems/AppMenu.java
+++ src/lt/vtmc/ems/AppMenu.java
@@ -49,7 +49,7 @@ public class AppMenu {
     }
 
     private void exitMenu() {
-        // TODO
+
         System.out.println("Continue (y/n): ");
         String read = input.next().toLowerCase();
         char close = read.charAt(0);
